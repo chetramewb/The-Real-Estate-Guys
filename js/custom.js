b859bfbd800d641a3976a79618d5b0f0f392930e
Chet Ram <56634108+chetramewb@users.noreply.github.com>
2020-10-12 17:41:55 +0530

    new style add

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,35 @@
 $('.slide-blog').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<button class="back-arrow"> <img src="./images/previous-arrow.png"> </button>', 
-  nextArrow: '<button class="next-arrow"><img src="./images/next-arrow.png"> </button>', 
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+dots: false,
+infinite: true,
+speed: 300,
+slidesToShow: 1,
+slidesToScroll: 1,
+Autoplay:true,
+prevArrow: '<button class="back-arrow"> <img src="./images/previous-arrow.png"> </button>', 
+nextArrow: '<button class="next-arrow"><img src="./images/next-arrow.png"> </button>', 
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
     }
-  ]
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
 });
